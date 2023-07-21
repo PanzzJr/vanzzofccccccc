084e2bc1469aff02a55a7ab65583ec749b74b28a
PanzzJr <134189028+PanzzJr@users.noreply.github.com>
2023-07-21 21:05:03 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'VanzzOFC'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://vanzjbcom:<IXev0FJiIkddzOBU>@cluster0.jqgodbp.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "vanzzjb@gmail.com" //email
-global.email_password = "" //application password email
+global.your_email = "naurakecil342@gmail.com" //email
+global.email_password = "noqqqneriexecrjh" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
